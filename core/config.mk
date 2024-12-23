011f21e3be2346ebfc83fc269ccdb11439f255d0
Yumi Yukimura <me.cafebabe@gmail.com>
2024-12-24 01:11:19 +0800

    build: Move inclusion of BoardConfigExtra.mk to board_config.mk
    Some kind of extras needs to alter variables that becomes read-only
    after inclusion of board_config.mk, or wants some variables getting
    processed by board_config.mk. For example, native bridge.
    
    Change-Id: If7ef0dcfc30b52940017662fa7402e4970dbc3c7

--- core/config.mk
+++ core/config.mk
@@ -473,8 +473,6 @@ FIND_LEAVES_EXCLUDES := $(addprefix --prune=, $(SCAN_EXCLUDE_DIRS) .repo .git)
 
 include vendor/aosp/config/BoardConfigQcom.mk
 
--include vendor/extra/BoardConfigExtra.mk
-
 # The build system exposes several variables for where to find the kernel
 # headers:
 #   TARGET_DEVICE_KERNEL_HEADERS is automatically created for the current
